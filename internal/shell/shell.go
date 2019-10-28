8473caeb9b16111a718fceb75e47b958e3b86538
Aleks Bulovic <aleks@stryda.dev>
2019-10-28 20:00:00 +1100

    Add foundational shell utilities

--- internal/shell/shell.go
+++ internal/shell/shell.go
@@ -3,68 +3,41 @@ package shell
 import (
 	"fmt"
 	"os"
+	"os/exec"
 )
-	//"os/exec"
 
-//	//"os/exec"
-//)
-//
-//func getCommand(command string, args []string) string {
-//	cmd := exec.Command("bash", "-c", "ps", "-p", "$$", "-ocomm=")
-//	//strings.Split(args, " ")...
-//	//args...
-//	output, err := cmd.CombinedOutput()
-//
-//	fmt.Println("Executing --->" + cmd.String())
-//
-//	if err != nil {
-//		log.Fatal("Failed to derive shell: " + err.Error())
-//	}
-//
-//	fmt.Println(string(output))
-//
-//	return string(output)
-//}
+func getCommand(cmd string, args []string, newShell bool) string {
+	if newShell {
+		args = append([]string{"-c", cmd}, args[0:]...)
+		cmd = "bash"
+	}
 
-func getEnvironment() []string {
+	out, err := exec.Command(cmd, args...).Output()
+	if err != nil {
+		return ""
+	}
+
+	return string(out)
+}
+
+func getEnv() []string {
 	return os.Environ()
 }
 
-func printEnvironment() {
-	for _, v := range getEnvironment() {
+func getEnvVariable(v string) string {
+	return os.Getenv(v)
+}
+
+func printEnv() {
+	for _, v := range getEnv() {
 		fmt.Println(v)
 	}
 }
 
-func getCurrentShell() string {
-	// ps -p $$ -oargs=
-	// ps -p $$ -ocomm=
-	//cmd := exec.Command("ps", "-p", "$$", "ocomm=")
-	fmt.Println("--------------------")
-	printEnvironment()
-	//sess := sh.Echo("ffs")
-
-	//xx, err := sess.Output()
-
-	// sh: count=$(echo "one two three" | wc -w)
-
-	val := os.ExpandEnv("$0")
-
-	os.Setenv("NAME", "gopher")
-	os.Setenv("BURROW", "/usr/gopher")
-
-	fmt.Println(os.ExpandEnv("$$ lives in ${BURROW}. "))
-
-
-
-
-	fmt.Println("OUT: " + val + "|")
-
-	return ""
-
-	//return getCommand("ps", []string{})
+func getShell() string {
+	return getCommand("ps", []string{"-p", "$$", "-ocomm="}, false)
 }
 
-func Print() {
-	getCurrentShell()
+func Print() string {
+	return getShell()
 }
